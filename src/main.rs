5d1fd71707afd2fcb0c19b135d25e4ffa56f7029
yu2ta7ka <yuji.tanaak@gmail.com>
2025-05-06 06:02:32 +0900

    Hello,worldの描画

--- src/main.rs
+++ src/main.rs
@@ -149,6 +149,7 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         draw_font_fg(&mut vram, i as i64 * 16 + 256, i as i64 * 16, 0xffffff, c);
     }
 
+    draw_str_fg(&mut vram, 256, 256, 0xffffff, "Hello, world!");
     //println!("Hello, world!");
     loop {
         hlt()
@@ -335,5 +336,11 @@ fn draw_font_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, c: char) {
                 let _ = draw_point(buf, color, x + dx as i64, y + dy as i64);
             }
         }
+    }
+}
+
+fn draw_str_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, s: &str) {
+    for (i, c) in s.chars().enumerate() {
+        draw_font_fg(buf, x + i as i64 * 8, y, color, c);
     }
 }
